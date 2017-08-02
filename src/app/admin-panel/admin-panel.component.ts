d4504bac3f536eed9ebd271d40e5ad809f90b3a8
Bryan Hokanson <hokanson.bryan@gmail.com>
2017-08-02 10:17:04 -0400

    fixed problem with tabs in the admin panel not showing right section on refresh

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 
+declare var jQuery: any;
+
 @Component({
   selector: 'sh-admin-panel',
   templateUrl: './admin-panel.component.html',
@@ -11,6 +13,24 @@ export class AdminPanelComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+
+    const url = this.router.url;
+
+    if (url === '/admin/users') {
+      jQuery('#userTab').addClass('active');
+    }
+
+    if (url === '/admin/providers') {
+      jQuery('#providerTab').addClass('active');
+    }
+
+    if (url === '/admin/organizations') {
+      jQuery('#organizationTab').addClass('active');
+    }
+
+    if (url === '/admin/measures') {
+      jQuery('#measuresTab').addClass('active');
+    }
 
   }
 
